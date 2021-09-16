5e805da7d193a3d19957be1acdfe7d290217da35
Liubchenko Vadim <v.liubchenko@pinta.com.ua>
2021-09-16 11:40:43 +0300

    Corrected notes

--- app/Services/EmployeeCheck.php
+++ app/Services/EmployeeCheck.php
@@ -1,30 +1,146 @@
 <?php
+
 namespace App\Services;
 
-use App\Models\Employee as EmployeeModel;
-use App\Models\EmployeeSkill;
-use App\Models\Skill;
+use App\Models\employeeAndSkill;
 
 class EmployeeCheck
 {
-    public function getSkillForEmployee($user)
+    private $skill = [
+        'writeCode' => 'code writing',
+        'testingCode' => 'code testing',
+        'communicationManager' => 'communication with manager',
+        'draw' => 'draw',
+        'setTasks' => 'set tasks',
+    ];
+
+    private $employeeAndSkill = [
+        'programmer' => [
+            'writeCode',
+            'testingCode',
+            'communicationManager',
+        ],
+        'designer' => [
+            'communicationManager',
+            'draw',
+        ],
+        'tester' => [
+            'communicationManager',
+            'testingCode',
+            'setTasks',
+        ],
+        'manager' => [
+            'setTasks',
+        ],
+    ];
+
+    private function isExistsSkill($skill)
+    {
+        if (!empty($skill) && isset($this->skill[$skill])) {
+            return true;
+        }
+        return false;
+    }
+
+    private function isExistsUser($user)
+    {
+        if (!empty($user) && isset($this->employeeAndSkill[$user])) {
+            return true;
+        }
+        return false;
+    }
+
+    private function getSkillName($key)
+    {
+        if (isset($this->skill[$key])) {
+            return $this->skill[$key];
+        }
+        return false;
+    }
+
+    private function getAllSkillForEmployee($employee)
+    {
+        $result = [];
+        if (isset($this->employeeAndSkill[$employee])) {
+            foreach ($this->employeeAndSkill[$employee] as $item) {
+                $name = $this->getSkillName($item);
+                if ($name) {
+                    $result[] = $name;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    private function isExistsSkillForEmployee($employee, $skill)
+    {
+        if (isset($this->employeeAndSkill[$employee]) && in_array($skill,$this->employeeAndSkill[$employee])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function getAllEmployee()
+    {
+        $result = [];
+        foreach ($this->employeeAndSkill as $key => $element) {
+            $result[] = $key;
+        }
+        return $result;
+    }
+
+    public function getCheckSkillExists($user, $skill)
     {
         $data = [];
-        $employee = EmployeeModel::where('name', $user)->first();
-        if (!is_null($employee)) {
-            $skils = EmployeeSkill::where('employee_id', $employee->id)->get();
-            $data['info'][] = "Employee {$user} skills:";
-            foreach ($skils as $skill) {
-                $name = Skill::find($skill->skill_id);
-                $data['info'][] = "- {$name->name}";
+        if (empty($user) || empty($skill)) {
+            $data['error'][] = "Parameters not corect!";
+            return $data;
+        }
+        if ($this->isExistsUser($user)) {
+            if ($this->isExistsSkill($skill)) {
+                $checked = $this->isExistsSkillForEmployee($user, $skill);
+                if (!empty($checked)) {
+                    $data['info'][] = "true";
+                } else {
+                    $data['error'][] = "false";
+                }
+            } else {
+                $data['error'][] = "Skill {$skill} not exists!";
+                $skills = $this->skill;
+                $data['info'][] = "Skill exists:";
+                foreach ($skills as $key => $item) {
+                    $data['info'][] = "-  key = {$key}";
+                }
             }
         } else {
             $data['error'][] = "Employee {$user} not exists!";
-            $employees = EmployeeModel::all();
+            $employees = $this->getAllEmployee();
+            $data['info'][] = "Employee exists:";
+            foreach ($employees as $employee) {
+                $data['info'][] = "- {$employee}";
+            }
+        }
+        return $data;
+    }
+
+    public function getSkillForEmployee($user)
+    {
+        $data = [];
+        if ($this->isExistsUser($user)) {
+            $skils = $this->getAllSkillForEmployee($user);
+            $data['info'][] = "Employee {$user} skills:";
+            foreach ($skils as  $skill) {
+                $data['info'][] = "- {$skill}";
+            }
+        } else {
+            $data['error'][] = "Employee {$user} not exists!";
+            $employees = $this->getAllEmployee();
             $data['info'][] = "Employee exists:";
 
             foreach ($employees as $employee) {
-                $data['info'][] = "- {$employee->name}";
+                $data['info'][] = "- {$employee}";
             }
         }
         return $data;
